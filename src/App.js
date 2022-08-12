7158ec8f02756257852e431eda89764c3f1f60c9
Kwabena <kwabenao2k@gmail.com>
2022-08-12 19:44:40 -0400

    Added the search bar & created function for obtaining the weatherData

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const key = "5783354b44651863d0bd15ae06c6a392";
+  const [weatherData, setWeather] = useState([{}]);
+  const [cityData, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityData}&appid=${key}&units=metric&lang=en`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeather(data);
+        });
+    }
+  };
 }
 
 export default App;
+
+// PSUEDOCODE
+// Create state items to hold data coming from the third-party API and the user input
+// - weather
+// - userInput from search bar
+
+// Once the component has been loaded call the local method (getWeather) to get weather data
+
+// A local method (getWeather) to make the third-party API call with or without user input
+// - when successful, update the state (weather) with new data
+// - if unsuccessful, display the error message
+
+// A local method (handleChange) to handle the onChange event to update state (userInput) with user input
+
+// Render the application
+// - header
+// - A search bar to allow the user to find a specific location
+// - use the imported Result component
+// - footer
+
+//  Result Part
+// Create a component to display data from the third-party API
+// This component will get data (weather) passed in as props
+// Based on userInput the call to the API will display the location and weather information

dc22573df0cc62220020d28b1f35585f936b2e2a
Kwabena <kwabenao2k@gmail.com>
2022-08-12 20:15:31 -0400

    Displaying relevant weather information to the screen and implemented error handling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  //Api key
   const key = "5783354b44651863d0bd15ae06c6a392";
   const [weatherData, setWeather] = useState([{}]);
   const [cityData, setCity] = useState("");
 
+  // Creating the call to the API to retrieve the data
   const getWeather = (event) => {
     if (event.key === "Enter") {
       fetch(
@@ -17,6 +19,34 @@ function App() {
         });
     }
   };
+
+  //console.log(weatherData);
+
+  return (
+    // Connecting the weatherData call from the API to the input form and also Displaying data from getWeather
+    <div className="wrapper">
+      <input
+        className="input"
+        placeholder="Please enter a location"
+        onChange={(event) => setCity(event.target.value)}
+        value={cityData}
+        onKeyPress={getWeather}
+      />
+
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>How's the weather looking right now?</p>
+        </div>
+      ) : (
+        <div>
+          <p>Location: {weatherData.name}</p>
+          <p>Temperature: {Math.round(weatherData.main.temp)}°C</p>
+          <p>Humidity: {weatherData.main.humidity}</p>
+          <p>Feels like: {weatherData.main.feels_like}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
